a2870894012f07df4dbf7157dc88fdfa0080d676
Karlo Skafec <karlo.skafec@ecx.io>
2022-04-10 12:15:08 +0200

    Removed a TODO

--- src/app/utils/util.ts
+++ src/app/utils/util.ts
@@ -4,6 +4,5 @@ export function generateCurrentWeatherApiUrl(
   latitude: number,
   longitude: number
 ): string {
-  // TODO: Don't commit this and add to GIT history, move the token! change units metric to be selected by user
   return `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${environment.apiKey}&units=metric`;
 }
